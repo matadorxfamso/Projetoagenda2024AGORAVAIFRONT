b26e8627a005f0550ee76d415201aafc3af4c3e2
MatheusPrazeres <matheusnathan320@gmail.com>
2024-12-09 19:23:12 -0300

    config da nav-bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { AgendaComponent } from './agenda/agenda.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  {path: '', redirectTo: '/inicio', pathMatch: 'full'},
+  {path: 'inicio', component: InicioComponent},
+  {path: 'agenda', component: AgendaComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
